c55992fc9179cae404134bbf5dc2669e3a2d0fd8
personal <mischalamoureux7@gmail.com>
2023-04-23 18:37:16 -0400

    Added typewriter effect + added portfolio image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,66 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from "../../assets/me.png";
+import MISCHA from "../../assets/mischa-lamoureux.png";
 import HeaderSocials from "./HeaderSocials";
+import Typewriter from "typewriter-effect";
 
 const Header = () => {
+  const shuffleArray = () => {
+    const originalArray = [
+      "Developer",
+      "Hiker",
+      "Biker",
+      "Climber",
+      "Bookworm",
+      "Tech Enthusiast",
+    ];
+    const shuffledArray = [...originalArray];
+
+    // remove 'Developer' from the array
+    const index = shuffledArray.indexOf("Developer");
+    if (index !== -1) {
+      shuffledArray.splice(index, 1);
+    }
+
+    // Fisher-Yates shuffle algorithm
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+
+    // add 'Developer' back to the beginning of the array
+    shuffledArray.unshift("Developer");
+
+    return shuffledArray;
+  };
+
   return (
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Mischa Lamoureux</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="header__typewriter">
+          I am a&nbsp;
+          <Typewriter
+            options={{
+              strings: shuffleArray(),
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h5>
         <CTA />
         <HeaderSocials />
 
-        <div className="me">{/* <img src={ME} alt="me" /> */}</div>
+        <div className="me">
+          <img src={MISCHA} alt="me" />
+        </div>
 
-        <a href="#contact" className="scroll__down">
+        <a href="#about" className="scroll__down">
           Scroll Down
         </a>
       </div>
