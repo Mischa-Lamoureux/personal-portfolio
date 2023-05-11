b88dd67e79bc43798effe9f35f2558152d71e5b8
Mischa Lamoureux <mischalamoureux7@gmail.com>
2023-05-10 21:02:08 -0400

    Removed Dribble

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
-import { FiDribbble } from "react-icons/fi";
 
 const HeaderSocials = () => {
   return (
@@ -20,9 +19,6 @@ const HeaderSocials = () => {
       >
         <FaGithub />
       </a>
-      {/* <a href="https://dribble.com" target="_blank" rel="noreferrer">
-        <FiDribbble />
-      </a> */}
     </div>
   );
 };
